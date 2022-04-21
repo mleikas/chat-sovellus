a7eb9348ec03dce69984175a5feae97a68d1a6e3
mleikas <max.o.leikas@helsinki.fi>
2022-04-21 19:59:11 +0300

    sending messages

--- messages.py
+++ messages.py
@@ -7,7 +7,7 @@ def get_list():
     return result.fetchall()
 
 def send(content):
-    user_id = users.user_id
+    user_id = users.user_id()
     if user_id == 0:
         return False
     sql = "INSERT INTO info (content, user_id, sent_at) VALUES (:content, :user_id, NOW())"
