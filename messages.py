d40d5bb359536483777dfb255006939db49ee24c
mleikas <max.o.leikas@helsinki.fi>
2022-04-23 18:06:23 +0300

    fixes with msgs showing where not supposed to

--- messages.py
+++ messages.py
@@ -3,7 +3,7 @@ from flask import session
 import users
 
 def get_list(thread_id):
-    sql = "SELECT I.content, U.username, I.sent_at FROM info I, users U WHERE I.user_id=U.id ORDER BY I.id DESC"
+    sql = "SELECT I.id, I.content, U.username, I.sent_at FROM info I, users U WHERE I.user_id=U.id AND thread_id=:id AND I.visibility=True ORDER BY I.id DESC"
     result = db.session.execute(sql)
     return result.fetchall()
 
