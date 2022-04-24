3ca63a67f701cb2703426391b17b9149e25f704c
mleikas <max.o.leikas@helsinki.fi>
2022-04-24 19:55:36 +0300

    bug fixes to message deletion

--- routing.py
+++ routing.py
@@ -42,10 +42,10 @@ def make_thread():
 @app.route("/delete", methods=["POST"])
 def delete():
     message_id = request.form["message_id"]
-    user_id = request.form["user_id"]
+    user_id = int(request.form["user_id"])
     thread_id = request.form["thread_id"]
-
-    if int(session["user_id"]) == user_id:
+    print(user_id)
+    if session["user_id"] == user_id:
         delete_message = messages.delete(message_id)
     if delete_message == False:
         return render_template("error.html", errormsg="Viestin poistaminen epäonnistui")
